4334d20c26dd6fa72746f0f20a6dfda2c8165065
piasanchob <55335112+piasanchob@users.noreply.github.com>
2020-09-26 21:45:55 -0600

    interfaz y main
    interfaz y main

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tienda;
+
+/**
+ *
+ * @author Andrey Sancho
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("welcome to the shop");
+        
+    }
+    
+}
