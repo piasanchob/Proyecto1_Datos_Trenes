705d3ff8da9dd9560fe4f07371026a1447beeb15
manuel1410 <71191544+manuel1410@users.noreply.github.com>
2020-10-10 14:30:18 -0600

    Add files via upload
    Al archivo de Items le agregué más items y a al archivo player le puse un método de comprar

--- item.java
+++ item.java
@@ -8,55 +8,21 @@ import java.util.List;
 import java.util.ArrayList;
 
 
-public class item{     //Estos son los atributos de la clase
-    private int atq;
-    private int def;
-    private int hp;
-    private int luk;
-    private int vel;
-
-
-    /*
-    La idea es que todos los items tengan todos los stats pero en caso de que no se ocupen,
-    se les pone 0. 
-
-    Por ejemplo un item de atq tendría def 0.
-    */
-
-
-    public item(int atq, int def, int hp, int lck, int vel){ //aquí va el constructor
-
-        super();
-
-        this.atq = atq;
-        this.def = def;
-        this.hp = hp;
-        this.luk = lck;
-        this.vel = vel;
-        
-        /*ArrayList<item> listaitemsatq = new ArrayList<item>();
-
-        item item01_atq = new item(50,0,5,0,5);
-        item item02_atq = new item(75,0,10,5,0);
-        item item03_atq = new item(85,10,25,0,0);
-        item item04_atq = new item(100,20,0,0,0);
-        item item05_atq = new item(105,0,15,25,5);
-
-
-        ArrayList<item> listaitemsdef = new ArrayList<item>();
-
-        item item01_def = new item(0,75,0,0,5);
-        item item02_def = new item(0,100,0,0,10);
-        item item03_def = new item(0,105,0,25,0);
-        item item04_def = new item(0,125,0,0,40);
-        item item05_def = new item(0,85,0,25,50);
-        System.out.println(listaitemsatq);
-        System.out.println(listaitemsdef);*/
-
-    }
+public class item{
     
-    //item item1 = new item(50,0,5,0,0);
-
-
-
+    int precio;
+    int statataque;
+    int statdefensa;
+    int statspeed;
+    int statluck;
+    int statsalud;
+    public item(int price, int statataque, int statdefensa, int statspeed,int statluck,int statsalud){
+        precio = price;
+        this.statataque = statataque;
+        this.statdefensa = statdefensa;
+        this.statluck = statluck;
+        this.statsalud = statsalud;
+        this.statspeed = statspeed;
+    }
 }
+    
--- player.java
+++ player.java
@@ -10,5 +10,43 @@ package com.mycompany.tienda;
  * @author Andrey Sancho
  */
 public class player {
+    int dinero;
+    int statataque;
+    int statdefensa;
+    int statspeed;
+    int statluck;
+    int statsalud;
+    public player(int dinero,int statataque, int statdefensa, int statspeed, int statluck,int statsalud){
+        this.dinero  = dinero;
+        this.statataque = statataque;
+        this.statdefensa = statdefensa;
+        this.statluck = statluck;
+        this.statsalud = statsalud;
+        this.statspeed = statspeed;
+    }
+    public void comprar(item precioproducto,item atq,item def,item speed,item luck,item salud){
+       dinero = dinero - precioproducto.precio;
+       statataque = statataque + atq.statataque;
+       statdefensa = statdefensa + def.statdefensa;
+       statspeed = statspeed + speed.statspeed;
+       statluck = statluck + luck.statluck;
+       statsalud = statsalud + salud.statsalud;
+       //PreguntarObando
+       
+    }
+    public void vender(inventory precioprodu){
+        dinero = dinero + precioprodu.precio;
+        //Eliminar item del inventario preguntar Obando
+        
+    }
+    public void imprimir(){
+        System.out.println("Dinero = " + dinero);
+        System.out.println("Ataque = " + statataque);
+        System.out.println("Defensa = " + statdefensa);
+        System.out.println("Velocidad = " + statspeed);
+        System.out.println("Suerte = " + statluck);
+        System.out.println("Salud = " + statsalud);
+        
+    }
     
 }
